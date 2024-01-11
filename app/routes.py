faa8306bc657d9d3912a8f28cb74c592dd41ac6e
Rizwan Halani <rizwan.halani@gmail.com>
2024-01-11 11:30:08 -0600

    added login page

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -16,7 +17,17 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-
-
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember me={}'.format(
+            form.username.data, form.password.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
+
+
+  
 
