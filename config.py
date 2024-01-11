cd90ac586852f7010e2f8455cf4a8c89e0e0ec5b
Rizwan Halani <rizwan.halani@gmail.com>
2024-01-11 12:53:33 -0600

    adding some files from the database section of the tutorial

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-class Config:
+class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     #add more variables here as needed
